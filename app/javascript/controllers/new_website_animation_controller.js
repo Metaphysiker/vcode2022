fdb3e15a2263fa318fb57c54cc69cfff2e2fa042
Metaphysiker <s.raess@me.com>
2022-09-10 11:02:58 +0200

    working on animation with svg

--- app/javascript/controllers/new_website_animation_controller.js
+++ app/javascript/controllers/new_website_animation_controller.js
@@ -4,7 +4,7 @@ import { useIntersection, useResize } from 'stimulus-use'
 
 export default class extends Controller {
   static targets = [ "name", "output", "animationField" ]
-  static values = { index: Number, drawn: Boolean, currentWidth: Number }
+  static values = { index: Number, drawn: Boolean, currentWidth: Number, svgUrl: String }
 
   connect() {
     this.currentWidthValue = this.animationFieldTarget.offsetWidth;
@@ -51,6 +51,7 @@ export default class extends Controller {
     let height = this.animationFieldTarget.offsetHeight; //width;
 
     let box = {width: width/1.5, height: width/1.5};
+    let svgUrl = this.svgUrlValue;
 
     // append the svg object to the body of the page
      const svg = d3.select(this.animationFieldTarget)
@@ -74,8 +75,102 @@ export default class extends Controller {
             .attr('width', box.width)
             .attr('height', box.height)
             .attr("fill", "#69a3b2")
-            .on("end", function() {console.log("end")});
+            .on("end", function() {
+              //append_navbar(append_title)
+              append_navbar()
+              .then(() => append_navbar_brand())
+              .then(() => append_navbar_menu())
+              .then(() => append_title())
+              .then(() => console.log("finish"));
+            });
             // end website_screen
+
+    function append_title() {
+
+      return new Promise(function(final_resolve, final_reject){
+
+      website_screen.append("text")
+        .attr("font-weight", 900)
+        .style('fill', 'white')
+        .style("font-size", "1.8em")
+        .attr("x", 0)
+        .attr("y", 0)
+        .text("webtastic")
+        .transition()
+              .duration(1000)
+              .attr("x", box.width/100 * 10)
+              .attr("y", box.height/100 * 20)
+              .on("end", function() {final_resolve()});
+            })
+    }
+
+    function append_navbar(){
+
+      return new Promise(function(final_resolve, final_reject){
+        website_screen.append('rect')
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr('width', 0)
+          .attr('height', 0)
+          .attr('fill', 'gray')
+          .transition()
+            .duration(1000)
+            .attr('width', box.width/100 * 100)
+            .attr('height', box.height/100 * 7.5)
+            .on("end", function() {final_resolve()});
+      })
+
+    }
+
+    function append_navbar_menu(){
+
+      return new Promise(function(final_resolve, final_reject){
+        website_screen.append('rect')
+          .attr("x", box.width/100 * 90)
+          .attr("y", 0)
+          .attr('width', 0)
+          .attr('height', 0)
+          .attr('fill', 'white')
+          .transition()
+            .duration(1000)
+            .attr('width', box.width/100 * 10)
+            .attr('height', box.height/100 * 1.5)
+            .on("end", function() {final_resolve()});
+      })
+
+    }
+
+    function append_navbar_brand(){
+
+      return new Promise(function(final_resolve, final_reject){
+
+        website_screen.append("svg:image")
+        .attr('x', box.width/100 * 1)
+        .attr('y', box.height/100 * 1)
+        .attr('width', box.width/100 * 10)
+        .attr('height', box.height/100 * 4.5)
+        .style("cursor", "pointer")
+        .style("opacity", 1)
+        .attr("xlink:href", svgUrl);
+
+
+        website_screen.append("text")
+          .attr("font-weight", 900)
+          .style("font-size", "1em")
+          .style('fill', 'white')
+          .attr("x", 0)
+          .attr("y", 0)
+          .text("brand")
+          .transition()
+                .duration(1000)
+                .attr("x", box.width/100 * 10)
+                .attr("y", box.height/100 * 5.5)
+                .on("end", function() {final_resolve()});
+      })
+
+    }
+
+
 
 
   } //end of draw
